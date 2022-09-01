9080eb304069b04c56781d0880dead95fc0fad5b
倒走的风景 <gaooooge@gmail.com>
2022-09-01 11:29:21 +0800

    商品折扣

---
+++ easy/finalPrices_test.go
@@ -0,0 +1,27 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func finalPrices(p []int) []int {
+	for i := 0; i < len(p); i++ {
+	OutFor:
+		for j := i + 1; j < len(p); j++ {
+			if p[i] > p[j] {
+				p[i] = p[i] - p[j]
+				break OutFor
+			}
+		}
+
+	}
+
+	return p
+}
+
+func TestFinalPrices(t *testing.T) {
+	p := []int{8, 4, 6, 2, 3}
+	res := finalPrices(p)
+	fmt.Println(res)
+}
