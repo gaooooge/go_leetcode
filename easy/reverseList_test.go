7a0b18e61df8fc3fba3f771742e553e649aef695
倒走的风景 <gaooooge@gmail.com>
2022-09-12 17:15:09 +0800

    学习进度更新

--- easy/reverseList_test.go
+++ easy/reverseList_test.go
@@ -3,7 +3,16 @@ package easy
 import "testing"
 
 func reverseList(head *ListNode) *ListNode {
-	
+	var prev *ListNode
+    curr := head
+    for curr != nil {
+        temp := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = temp
+    }
+
+    return prev
 }
 
 func TestReverseList(t *testing.T) {
