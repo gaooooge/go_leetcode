a7e808f748bd9e0b319caa86224f3bcd026626ad
倒走的风景 <gaooooge@gmail.com>
2022-09-14 10:07:46 +0800

    验证二叉搜索树

---
+++ easy/hasCycle_test.go
@@ -0,0 +1,37 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func hasCycle(head *ListNode) bool {
+	hash := map[*ListNode]bool{}
+
+	for head != nil {
+		if ok := hash[head]; ok {
+			return true
+		}
+		head = head.Next
+		hash[head] = true
+	}
+
+	return false
+}
+
+func SetList(nums []int) *ListNode {
+	var prev *ListNode
+	for i := len(nums) - 1; i >= 0; i-- {
+		var a *ListNode
+		a.Val = nums[i]
+		a.Next = prev
+		prev = a
+	}
+	
+}
+
+func TestHasCycle(t *testing.T) {
+	l := SetList([]int{1, 2, 3, 4, 5})
+	fmt.Println(l)
+	// res :=
+}
---
+++ easy/isValidBST_test.go
@@ -0,0 +1,24 @@
+package easy
+
+import "math"
+
+func isValidBST(root *TreeNode) bool {
+	return helper(root,math.MinInt64,math.MaxInt64)
+}
+
+func helper(root *TreeNode, lower,upper int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val >= upper || root.Val <= lower {
+		return false
+	}
+
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
+}
+
+
+func TestIsValidBST(t *testing.T) {
+
+}
