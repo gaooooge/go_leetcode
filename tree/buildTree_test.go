5809f6942b5d3e24057a6d7085cafede31c0ba64
倒走的风景 <gaooooge@gmail.com>
2022-09-28 08:53:02 +0800

    从中序和后序遍历构造二叉树

---
+++ tree/buildTree_test.go
@@ -0,0 +1,37 @@
+package treeNode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TreeNode struct {
+	Val   int       //val
+	Left  *TreeNode //left
+	Right *TreeNode //right
+}
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(inorder) == 0 || len(postorder) == 0 {
+		return nil
+	}
+	rv := postorder[len(postorder)-1]
+	var i int
+	for i := range inorder {
+		if rv == inorder[i] {
+			break
+		}
+	}
+	return &TreeNode{
+		Val: rv,
+		Left: buildTree(inorder[:i], postorder[:i]),
+		Right: buildTree(inorder[i+1:], postorder[i:len(postorder)-1]),
+	}
+}
+
+func TestBuildTree(t *testing.T) {
+	inorder := []int{9, 3, 15, 20, 7}
+	postorder := []int{9, 15, 7, 20, 3}
+	res := buildTree(inorder, postorder)
+	fmt.Println(res)
+}
